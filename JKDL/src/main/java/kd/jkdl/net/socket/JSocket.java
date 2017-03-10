3c082fae58082c28b363ead9a37ede75445b77ff
Sejoslaw <k.dobrzynski94@gmail.com>
2017-03-10 20:00:28 +0100

    Added new JSocket.

---
+++ JKDL/src/main/java/kd/jkdl/net/socket/JSocket.java
@@ -0,0 +1,54 @@
+package main.java.kd.jkdl.net.socket;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * @author <a href="mailto:k.dobrzynski94@gmail.com">Krzysztof Dobrzyñski</a> -> https://github.com/Sejoslaw
+ */
+public class JSocket 
+{
+	/**
+	 * @return Return new ServerSocket.
+	 * 
+	 * @throws IOException
+	 * 
+	 * @see ServerSocket
+	 */
+	public static ServerSocket newServerSocket(int port) 
+			throws IOException
+	{
+		return new ServerSocket(port);
+	}
+	
+	/**
+	 * @return Return new InetAddress.
+	 * 
+	 * @throws UnknownHostException
+	 * 
+	 * @see {@link InetAddress#getByName(String)}
+	 * @see InetAddress
+	 */
+	public static InetAddress newInetAddress(String host) 
+			throws UnknownHostException
+	{
+		return InetAddress.getByName(host);
+	}
+	
+	/**
+	 * @return Return new Socket.
+	 * 
+	 * @throws UnknownHostException
+	 * @throws IOException
+	 * 
+	 * @see Socket
+	 */
+	public static Socket newSocket(String host, int port) 
+			throws UnknownHostException, IOException
+	{
+		return new Socket(host, port);
+	}
+}
---
+++ JKDL/src/test/net/TestSocketServer.java
@@ -0,0 +1,47 @@
+package test.net;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+import main.java.kd.jkdl.net.socket.JSocket;
+
+/**
+ * A TCP server that runs on port 9090.  When a client connects, it
+ * sends the client the current date and time, then closes the
+ * connection with that client.  Arguably just about the simplest
+ * server you can write.
+ */
+public class TestSocketServer
+{
+	/**
+     * Runs the server.
+     */
+    public static void main(String[] args) 
+    		throws IOException 
+    {
+        ServerSocket listener = JSocket.newServerSocket(9090);
+        try 
+        {
+            while(true) 
+            {
+                Socket socket = listener.accept();
+                try 
+                {
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                    out.println(new Date().toString());
+                } 
+                finally 
+                {
+                    socket.close();
+                }
+            }
+        }
+        finally 
+        {
+            listener.close();
+        }
+    }
+}
