15a90dc7442cbddfd2780a42b44517b941493e90
Sejoslaw <k.dobrzynski94@gmail.com>
2017-03-12 14:14:53 +0100

    Added few @see to JFileExplorer

--- JKDL/src/main/java/kd/jkdl/component/JFileExplorer.java
+++ JKDL/src/main/java/kd/jkdl/component/JFileExplorer.java
@@ -1,11 +1,21 @@
 package main.java.kd.jkdl.component;
 
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileSystemView;
+import javax.swing.filechooser.FileView;
+
 /**
  * TODO: Write a File Explorer which will show 2 windows in one Frame.
  * This Frame will have menu bar, 2 windows and few buttons.
  * It will also be a component available to add to any new Frame.
  * 
  * @author <a href="mailto:k.dobrzynski94@gmail.com">Krzysztof Dobrzyñski</a> -> https://github.com/Sejoslaw
+ * 
+ * @see JFileChooser
+ * @see FileView
+ * @see JDialog
+ * @see FileSystemView
  */
 public class JFileExplorer
 {
