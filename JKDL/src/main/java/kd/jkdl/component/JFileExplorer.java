141c724f5648320c40ebf8b1dbdb2e225a70ff79
Sejoslaw <k.dobrzynski94@gmail.com>
2017-03-12 14:10:49 +0100

    Some TODOs cleaning

---
+++ JKDL/src/main/java/kd/jkdl/component/JFileExplorer.java
@@ -0,0 +1,12 @@
+package main.java.kd.jkdl.component;
+
+/**
+ * TODO: Write a File Explorer which will show 2 windows in one Frame.
+ * This Frame will have menu bar, 2 windows and few buttons.
+ * It will also be a component available to add to any new Frame.
+ * 
+ * @author <a href="mailto:k.dobrzynski94@gmail.com">Krzysztof Dobrzyñski</a> -> https://github.com/Sejoslaw
+ */
+public class JFileExplorer
+{
+}
---
+++ JKDL/src/main/java/kd/jkdl/logicgate/ILogicGate.java
@@ -0,0 +1,33 @@
+package main.java.kd.jkdl.logicgate;
+
+/**
+ * TODO: Create all basic logic gates.
+ * 
+ * Determines basics of each logic gate.
+ * 
+ * @author <a href="mailto:k.dobrzynski94@gmail.com">Krzysztof Dobrzyñski</a> -> https://github.com/Sejoslaw
+ */
+public interface ILogicGate<T>
+{
+	/**
+	 * @param object Object which will be determined.
+	 * 
+	 * @return Return TRUE if given object can be treated as Zero, otherwise FALSE.
+	 */
+	public abstract boolean isZero(T object);
+	
+	/**
+	 * @param object Object which will be determined.
+	 * 
+	 * @return Return TRUE if given object can be treated as One, otherwise FALSE.
+	 */
+	public abstract boolean isOne(T object);
+	
+	/**
+	 * @param input Input objects.
+	 * 
+	 * @return Return the result of this logic gate based on given input.
+	 */
+	@SuppressWarnings("unchecked")
+	public abstract T result(T... input);
+}
