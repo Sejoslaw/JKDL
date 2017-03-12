5cf0e66c0cbfa5fb5f82c84669a47f6752c4f8f1
Sejoslaw <k.dobrzynski94@gmail.com>
2017-03-12 15:43:12 +0100

    Cleaned up JSQL comment

--- JKDL/src/main/java/kd/jkdl/sql/JSQL.java
+++ JKDL/src/main/java/kd/jkdl/sql/JSQL.java
@@ -3,7 +3,7 @@ package main.java.kd.jkdl.sql;
 /**
  * TODO: Add support for Java build-in SQL API.
  * 
- * This class contains various methods to operate on SQL API.
+ * This class contains various methods to operate on Java SQL API.
  * 
  * @author <a href="mailto:k.dobrzynski94@gmail.com">Krzysztof Dobrzyñski</a> -> https://github.com/Sejoslaw
  */
