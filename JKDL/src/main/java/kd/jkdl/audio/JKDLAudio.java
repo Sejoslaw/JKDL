f06d9a604d5eed027f26a94c1035b07de46e8f10
Sejoslaw <k.dobrzynski94@gmail.com>
2017-03-13 10:04:43 +0100

    Added few Deprecated methods to JKDLAudio

--- JKDL/src/main/java/kd/jkdl/audio/JKDLAudio.java
+++ JKDL/src/main/java/kd/jkdl/audio/JKDLAudio.java
@@ -1,14 +1,28 @@
 package main.java.kd.jkdl.audio;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
 import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFileFormat.Type;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.Mixer;
 import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.TargetDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import sun.audio.AudioData;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+import sun.audio.ContinuousAudioDataStream;
 
 /**
  * This class contains various methods to operate on Java Audio API. <br>
@@ -30,6 +44,135 @@ public class JKDLAudio
 {
 	private JKDLAudio()
 	{
+	}
+	
+	/**
+	 * Stop playing audio from given InputStream.
+	 * 
+	 * @see InputStream
+	 */
+	@Deprecated
+	public static void stopAudio(InputStream inputStream)
+	{
+		AudioPlayer.player.stop(inputStream);
+	}
+	
+	/**
+	 * Stop playing audio from given File.
+	 * 
+	 * @see AudioInputStream
+	 */
+	@Deprecated
+	public static void stopAudio(File file) 
+			throws UnsupportedAudioFileException, IOException
+	{
+		AudioInputStream ais = AudioSystem.getAudioInputStream(file);
+		AudioPlayer.player.stop(ais);
+	}
+	
+	/**
+	 * Stop playing audio in loop from given ContinuousAudioDataStream.
+	 * 
+	 * @see ContinuousAudioDataStream
+	 */
+	@Deprecated
+	public static void stopLoopAudio(ContinuousAudioDataStream cas)
+	{
+		AudioPlayer.player.stop(cas);
+	}
+	
+	/**
+	 * Play audio from given File.
+	 * 
+	 * @see AudioInputStream
+	 */
+	@Deprecated
+	public static void playAudio(File file) 
+			throws UnsupportedAudioFileException, IOException
+	{
+		AudioInputStream ais = AudioSystem.getAudioInputStream(file);
+		AudioPlayer.player.start(ais);
+	}
+	
+	/**
+	 * Play audio from given InputStream.
+	 * 
+	 * @see InputStream
+	 */
+	@Deprecated
+	public static void playAudio(InputStream inputStream)
+	{
+		AudioPlayer.player.start(inputStream);
+	}
+	
+	/**
+	 * Play audio in a loop from given ContinuousAudioDataStream.
+	 * 
+	 * @see ContinuousAudioDataStream
+	 */
+	@Deprecated
+	public static void loopAudio(ContinuousAudioDataStream cas)
+	{
+		AudioPlayer.player.start(cas);
+	}
+	
+	/**
+	 * @return Return new ContinuousAudioDataStream
+	 * 
+	 * @see ContinuousAudioDataStream
+	 */
+	@Deprecated
+	public static ContinuousAudioDataStream newContinuousAudioDataStream(AudioData data)
+	{
+		return new ContinuousAudioDataStream(data);
+	}
+	
+	/**
+	 * @return Return AudioData from given AudioStream.
+	 * 
+	 * @see AudioData
+	 */
+	@Deprecated
+	public static AudioData getAudioData(AudioStream stream) 
+			throws IOException
+	{
+		return stream.getData();
+	}
+	
+	/**
+	 * @return Return new AudioStream.
+	 * 
+	 * @see AudioStream
+	 */
+	@Deprecated
+	public static AudioStream newAudioStream(URL url) 
+			throws IOException
+	{
+		return new AudioStream(url.openStream());
+	}
+	
+	/**
+	 * @return Return new AudioStream.
+	 * 
+	 * @see AudioStream
+	 */
+	@Deprecated
+	public static AudioStream newAudioStream(File file) 
+			throws FileNotFoundException, IOException
+	{
+		return new AudioStream(new FileInputStream(file));
+	}
+	
+	/**
+	 * Play WAV File.
+	 */
+	public static void playWAV(File wavFile) 
+			throws UnsupportedAudioFileException, IOException, LineUnavailableException
+	{
+		AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(wavFile);
+		Clip clip = AudioSystem.getClip();
+		clip.open(audioInputStream);
+		clip.start();
 	}
 	
 	/**
